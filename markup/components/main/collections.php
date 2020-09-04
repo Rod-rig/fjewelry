8307ede869c93b39f2091abe4117890b526ba5e2
Rod-rig <cherniakvadim08@gmail.com>
2020-09-04 23:39:10 +0300

    collections+trending blocks

---
+++ markup/components/main/collections.php
@@ -0,0 +1,40 @@
+<div class="collections">
+  <div class="container">
+    <div class="collections__row">
+      <div class="collections__column">
+        <div class="collections__item">
+          <a href="" class="collections__link">
+            <div>
+              <img class="img-responsive" src="https://via.placeholder.com/700x900" alt="">
+            </div>
+            <div class="collections__name">Kriti Sanon’s collection</div>
+          </a>
+        </div>
+        <div class="collections__all_mob">
+          <a href="" class="collections__all">All collections</a>
+        </div>
+      </div>
+      <div class="collections__column">
+        <div class="collections__item">
+          <a href="" class="collections__link">
+            <div>
+              <img class="img-responsive" src="https://via.placeholder.com/700x350" alt="">
+            </div>
+            <div class="collections__name">New Disney collection</div>
+          </a>
+        </div>
+        <div class="collections__item">
+          <a href="" class="collections__link">
+            <div>
+              <img class="img-responsive" src="https://via.placeholder.com/700x600" alt="">
+            </div>
+            <div class="collections__name">New Super collection</div>
+          </a>
+        </div>
+      </div>
+    </div>
+    <div class="collections__all_row">
+      <a href="" class="collections__all">All collections</a>
+    </div>
+  </div>
+</div>
--- markup/pages/main.php
+++ markup/pages/main.php
@@ -3,4 +3,6 @@
 <?php includeComponent('main/carousel', 'carousel/last-watched'); ?>
 <?php require '../components/main/discover.php'; ?>
 <?php includeComponent('main/carousel', 'carousel/best'); ?>
+<?php require '../components/main/collections.php'; ?>
+<?php includeComponent('main/carousel', 'carousel/trend'); ?>
 <?php require '../footer.php'; ?>
