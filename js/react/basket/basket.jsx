725064610cf209d09aff32ee57b2f6f7ede9fec4
Rod-rig <cherniakvadim08@gmail.com>
2020-11-09 17:59:48 +0200

    add mob basket delivery section

--- js/react/basket/basket.jsx
+++ js/react/basket/basket.jsx
@@ -39,17 +39,81 @@ export const Basket = () => {
 
   return isLoaded ? (
     basket[PRODUCTS] && basket[PRODUCTS].length > 0 ? (
-      <React.Fragment>
-        {basket[PRODUCTS].map(p => (
-          <BasketItem
-            setActiveSize={size => setActiveSize(p.id, size)}
-            setQuantity={value => setQuantity(p.id, value)}
-            key={p.id}
-            data={p}
-            labels={basket["labels"]}
-          />
-        ))}
-      </React.Fragment>
+      <div className="">
+        <div className="">
+          {basket[PRODUCTS].map(p => (
+            <BasketItem
+              setActiveSize={size => setActiveSize(p.id, size)}
+              setQuantity={value => setQuantity(p.id, value)}
+              key={p.id}
+              data={p}
+              labels={basket["labels"]}
+            />
+          ))}
+        </div>
+        <div className="basket__right">
+          <div className="basket__delivery">
+            {basket["delivery"].map(d => (
+              <div key={d.name} className="basket__delivery_radio">
+                <input
+                  checked={d.checked}
+                  className="basket__delivery_input"
+                  name="delivery"
+                  type="radio"
+                  onChange={e => {
+                    console.log(e.target.checked);
+                  }}
+                />
+                <div className="basket__delivery_body">
+                  <div className="basket__delivery_content">
+                    <svg className="basket__delivery_icon">
+                      <use xlinkHref={`#${d.icon}`} />
+                    </svg>
+                    <div className="basket__delivery_name">{d.name}</div>
+                  </div>
+                  <div className="basket__delivery_schedule">{d.schedule}</div>
+                </div>
+              </div>
+            ))}
+            <div className="mt-20">
+              <div className="basket__delivery_row">
+                <div className="basket__delivery_price">
+                  {basket.labels.price}
+                </div>
+                <div className="basket__delivery_price">
+                  {basket.main.price}
+                </div>
+              </div>
+              <div className="basket__delivery_row">
+                <div className="basket__delivery_price">
+                  {basket.labels.delivery}
+                </div>
+                <div className="basket__delivery_price">
+                  {basket.main.delivery}
+                </div>
+              </div>
+            </div>
+            <div className="basket__delivery_total">
+              <div className="basket__delivery_row">
+                <div className="basket__delivery_price basket__delivery_price--total">
+                  {basket.labels.total}
+                </div>
+                <div className="basket__delivery_price">
+                  {basket.main.total}
+                </div>
+              </div>
+              <div className="basket__delivery_schedule">
+                {basket.labels.totalNote}
+              </div>
+            </div>
+            <div className="basket__delivery_footer">
+              <a href={basket.hrefs.submit} className="basket__delivery_submit">
+                {basket.labels.secure}
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
     ) : (
       <div>{basket["labels"]["empty"]}</div>
     )
