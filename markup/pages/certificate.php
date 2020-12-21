40fc9622dd1fd00fe202df52e230fa192d78922b
Rod-rig <cherniakvadim08@gmail.com>
2020-12-22 00:05:20 +0200

    add certificate page

---
+++ markup/pages/certificate.php
@@ -0,0 +1,25 @@
+<?php require '../header.php'; ?>
+<?php require '../components/common/breadcrumbs.php'; ?>
+
+<div class="container content">
+  <h1 class="h1">FJewellery Certificate</h1>
+  <div class="certificate">
+    <div>
+      <p>All our items above £200 will come with a complimentary Authenticity Certificate / Insurance Valuation stating the recommended retail price for insurance replacement.</p>
+      <p>Professional gemmologists and jewellery experts do our valuations, members of the Gemmological Association of Great Britain (Gem-a) and National Association of jewellers (NAJ).</p>
+      <p>The certificate will include a detailed description of the jewellery including metal type, gemstone information, measurements etc.</p>
+      <div class="certificate__row">
+        <svg class="certificate__icon">
+          <use xlink:href="#info"></use>
+        </svg>
+        <div class="certificate__text">"We always recommend arranging 			insurance cover for your jewellery so 		you don't need to worry when you 			are wearing and enjoying your 			jewellery."</div>
+      </div>
+      <p>F Jewellery certificate will also serve as a document for our one-year warranty.</p>
+    </div>
+    <div class="certificate__img">
+      <img src="<?= SITE_TEMPLATE_PATH; ?>/img/certificate.jpg" class="img-responsive" />
+    </div>
+  </div>
+</div>
+
+<?php require '../footer.php'; ?>
