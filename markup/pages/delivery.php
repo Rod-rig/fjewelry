bae6b88c072768ad3350875a4448a429288ae4cb
Rod-rig <cherniakvadim08@gmail.com>
2020-12-22 15:12:23 +0200

    add delivery page

---
+++ markup/pages/delivery.php
@@ -0,0 +1,93 @@
+<?php require '../header.php'; ?>
+<?php require '../components/common/breadcrumbs.php'; ?>
+
+<div class="container content">
+  <h1 class="h1">Delivery Information</h1>
+  <div class="delivery">
+    <div class="delivery__services">
+      <div class="delivery__info">
+        <svg class="delivery__info_icon">
+          <use xlink:href="#info" />
+        </svg>
+        <div>Please note that Delivery is:</div>
+      </div>
+      <div class="delivery__li">Monday - Saturday (excluding public holidays)</div>
+      <div class="delivery__li">A signature may be required on receipt</div>
+      <div class="delivery__table_title">Uk standard delivery</div>
+      <table class="delivery__table">
+        <tr>
+          <td>Delivery</td>
+          <td class="text-right" colspan="2">2 - 5 working days</td>
+        </tr>
+        <tr>
+          <td>Total spent</td>
+          <td class="text-center">Up to £49</td>
+          <td class="text-right">Over £49</td>
+        </tr>
+        <tr>
+          <td>Charges</td>
+          <td class="text-center">£3.5</td>
+          <td class="text-right">
+            <div class="delivery__free">FREE</div>
+          </td>
+        </tr>
+      </table>
+      <div class="delivery__table_title">Uk express delivery</div>
+      <table class="delivery__table">
+        <tr>
+          <td>Delivery</td>
+          <td class="text-right" colspan="2">1 working day</td>
+        </tr>
+        <tr>
+          <td>Total spent</td>
+          <td class="text-center">Up to £250</td>
+          <td class="text-right">Over £250</td>
+        </tr>
+        <tr>
+          <td>Charges</td>
+          <td class="text-center">£5.95</td>
+          <td class="text-right">
+            <div class="delivery__free">FREE</div>
+          </td>
+        </tr>
+      </table>
+      <div class="delivery__table_title">International Delivery (Europe only)</div>
+      <div class="delivery__li">Maybe considerably longer for remote areas</div>
+      <div class="delivery__li">A signature may be required on receipt</div>
+      <table class="delivery__table">
+        <tr>
+          <td>Delivery</td>
+          <td class="text-right" colspan="2">3 - 5 working days (minimum)</td>
+        </tr>
+        <tr>
+          <td>Total spent</td>
+          <td class="text-center">Up to £250</td>
+          <td class="text-right">Over £250</td>
+        </tr>
+        <tr>
+          <td>Charges</td>
+          <td class="text-center">£5.95</td>
+          <td class="text-right">
+            <div class="delivery__free">FREE</div>
+          </td>
+        </tr>
+      </table>
+    </div>
+    <div class="delivery__content">
+      <div class="content__h2">DELIVERY REQUIREMENTS</div>
+      <p>All orders over £95 require a signature on delivery for security reasons. Fjewellery Limited may choose to send certain orders via Express Delivery even if you have not chosen it (at no extra cost to you). This would typically occur with orders for larger or heavier items such as clocks and jewellery boxes. This is a guaranteed <span class="delivery__free">next working day</span> delivery after despatch - a Saturday delivery <span class="delivery__underline">is not guaranteed</span>.</p>
+      <div class="content__h2">EXPRESS DELIVERY</div>
+      <p>Express is a next day delivery service for selected orders placed <span class="delivery__free">before 3PM</span>, Monday to Friday. Selected orders placed after <span class="delivery__free">3PM Monday to Thursday</span> should arrive in 2 days. Selected orders placed <span class="delivery__free">between 3PM Friday and 3PM Sunday</span>, will arrive on the following <span class="delivery__free">Monday</span>.</p>
+      <p>Delivery usually takes place between 8AM – 6PM. Orders for express delivery will need to be signed for.</p>
+      <p>Please note, delivery times to the Scottish Highlands & Islands, Northern Ireland, Isle of Wight and the Scilly Isles will vary and may take longer than expected, even if Express Delivery is selected. Fjewellery Limited will aim to select the most suitable carrier with the fastest delivery time, at a reasonable cost to us. Please contact us on 0800 363 6536 for more details. You will be provided with estimated delivery dates for your order in checkout.</p>
+      <div class="delivery__info delivery__note">
+        <svg class="delivery__info_icon">
+          <use xlink:href="#info" />
+        </svg>
+        <div><span class="text-bold">Please note:</span> Delivery times are prolonged on public holidays. On public holidays, 		please refer to the expected delivery dates provided in checkout. Delivery times for		items ordered during busy periods could take up to 2 days longer than usual. </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<?php require '../footer.php'; ?>
